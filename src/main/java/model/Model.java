2b6672d7e69c212f81b67888b4615be2e12e70c6
Delouya1 <delouya1@gmail.com>
2023-04-22 12:16:28 +0300

    Created the properties.txt to use as a setting file. Created the struct of the Model class and implemented the Ctor.

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,9 +1,42 @@
 package model;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Random;
 
 public class Model extends Observable {
+
+    HashMap<String,String> properties;  // map name of the prop to is value name
+    Socket fg;
+    PrintWriter outToFG;
+
+    public Model(String propertiesFileName) {
+        properties = new HashMap<>();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(propertiesFileName));
+            String line;
+            while ((line = in.readLine()) != null){
+                String[] sp = line.split(",");
+                properties.put(sp[0],sp[1]);
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int port = Integer.parseInt(properties.get("port"));
+
+        try {
+            fg = new Socket(properties.get("ip"),port);
+            outToFG = new PrintWriter(fg.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void setAileron(double x){
 
@@ -17,5 +50,15 @@ public class Model extends Observable {
     public void setThrottle(double x){
 
     }
+
+    public void close(){
+        try {
+            outToFG.close();
+            fg.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
 
